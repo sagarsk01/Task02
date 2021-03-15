603b6c8e7024dddcc1d4e6cf5a902470a9ceadba
sagarsk01 <80068476+sagarsk01@users.noreply.github.com>
2021-03-15 15:53:59 +0530

    Add files via upload

---
+++ Installation.ps1
@@ -0,0 +1,9 @@
+﻿   Get-Host
+	Get-WindowsFeature
+	Add-WindowsFeature -name Web-Server
+	start inetmgr
+	Get-WindowsFeature -name *web*
+	Install-WindowsFeature Web-Mgmt-Console
+	start inetmgr
+	Install-WindowsFeature -name Web-Ftp-Server
+	start inetmgr
